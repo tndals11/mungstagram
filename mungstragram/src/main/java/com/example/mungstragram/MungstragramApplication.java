4e4bec7d5fac9e97800fc5d0060db50e5d26cee4
tndals11 <tmdals09231@naver.com>
2026-02-02 02:38:19 +0900

    환경셋팅

---
+++ mungstragram/src/main/java/com/example/mungstragram/MungstragramApplication.java
@@ -0,0 +1,13 @@
+package com.example.mungstragram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungstragramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MungstragramApplication.class, args);
+    }
+
+}
---
+++ mungstragram/src/test/java/com/example/mungstragram/MungstragramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mungstragram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungstragramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
